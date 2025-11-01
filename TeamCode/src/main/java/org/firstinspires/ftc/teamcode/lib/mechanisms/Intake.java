e720068446cce1432e1e45e43dd1204b64e12215
team25674 <team25674@broomfieldstem.org>
2025-11-01 10:02:12 -0600

    Update Intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/Intake.java
@@ -6,9 +6,22 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class Intake {
     private LinearOpMode opMode = null;
     private DcMotor motor = null;
+    private final static double POWER = 0.75;
     public Intake(LinearOpMode OpMode) {
         opMode = OpMode;
-        this.motor = opMode.hardwareMap.get(DcMotor.class, "Jimy");
+        this.motor = opMode.hardwareMap.get(DcMotor.class, "Jimmy");
 
     }
+
+    public void start() {
+        while (this.opMode.opModeIsActive()) {
+            //1. read inputs
+            //2. process
+            //3. set outputs
+            if(this.opMode.gamepad2.right_bumper){
+            this.motor.setPower(POWER);
+            }
+        }
+    }
+
 }
