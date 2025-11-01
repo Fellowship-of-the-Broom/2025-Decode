88af6a52efdbe3bf8baf1ada60ad97a5a218a91f
team25674 <team25674@broomfieldstem.org>
2025-11-01 13:14:08 -0600

    added new thread in Intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/Intake.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-public class Intake {
+public class Intake implements Runnable {
     private LinearOpMode opMode = null;
     private DcMotor motor = null;
     private final static double POWER = 0.75;
@@ -16,15 +16,21 @@ public class Intake {
     }
 
     public void start() {
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+
         while (this.opMode.opModeIsActive()) {
             //1. read inputs
             //2. process
             //3. set outputs
             this.motor.setDirection(DcMotorSimple.Direction.FORWARD);
-            if(this.opMode.gamepad2.right_bumper){
-            this.motor.setPower(POWER);
+            if (this.opMode.gamepad2.right_bumper) {
+                this.motor.setPower(POWER);
             }
         }
     }
-
 }
