4ff0cf346549ae1da19f26b291af0e4f48e3ff44
team25674 <team25674@broomfieldstem.org>
2025-12-02 19:09:09 -0700

    Added code for the transfer gate!!! :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/TransferSystemImpl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/TransferSystemImpl.java
@@ -2,36 +2,55 @@ package org.firstinspires.ftc.teamcode.lib.mechanisms;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class TransferSystemImpl implements TransferSystem {
-    public static final double BLOCK_ANGLE = .85;
-    public static final double CLEAR_ANGLE = .85;
+    public static final double BLOCK_ANGLE = 0;
+    public static final double CLEAR_ANGLE = 0;
     private final double STOP_SPEED = 0.5;
     private LinearOpMode opMode = null;
-
-
-
-    private Servo transferFinger;
+    private Servo transferGate;
+    private GateStates gateState;
+    private ElapsedTime timeElapsed = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    private final double TIME_TO_CLOSE = 1500;
 
     public TransferSystemImpl(LinearOpMode OpMode) {
         this.opMode = OpMode;
 
-        this.transferFinger = opMode.hardwareMap.get(Servo.class, "transferWheel");
+        this.close();
+
+        this.transferGate = opMode.hardwareMap.get(Servo.class, "transferWheel");
     }
     @Override
     public void run() {
 
-        transferFinger.setDirection(Servo.Direction.FORWARD);
+        transferGate.setDirection(Servo.Direction.FORWARD);
 
         if(opMode.gamepad2.a){
-           transferFinger.setPosition(CLEAR_ANGLE);
-        } else {
-           transferFinger.setPosition(BLOCK_ANGLE);
+            this.open();
+        }
+        else if (timeElapsed.time() >= TIME_TO_CLOSE){
+            this.close();
         }
     }
+
     public void stop() {
-        transferFinger.setPosition(STOP_SPEED);
+        transferGate.setPosition(STOP_SPEED);
+    }
+
+    private void close(){
+        this.gateState = GateStates.CLOSED;
+        this.transferGate.setPosition(BLOCK_ANGLE);
+    }
+
+    private void open(){
+        this.gateState = GateStates.OPEN;
+        this.transferGate.setPosition(CLEAR_ANGLE);
+        this.timeElapsed.reset();
+        this.timeElapsed.time();
+    }
+    private static enum GateStates{
+        OPEN,
+        CLOSED
     }
 }
-
-
