a8583cc4b3934aa0ee5dd691fa16f0bfe266fd8b
team25674 <team25674@broomfieldstem.org>
2026-06-09 20:28:44 -0600

    Cool Beans!!!!!!!!!!!!!!!!!!!!
    We changed the pose.getX to "shortcut" to the pinpoint hardware for the telemetry. We also rewired the robot and that helped with the input since the control hub uses the first I2C port for the internal IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/production/OdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/production/OdometryTest.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.production;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 
 @TeleOp(name = "Odometry Test", group = "Test")
@@ -19,14 +22,20 @@ public class OdometryTest extends LinearOpMode {
         while (opModeIsActive()) {
 
             drive.update();
+            GoBildaPinpointDriver pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
 
-            Pose2d pose = drive.getPoseEstimate();
+//            Pose2d pose = drive.getPoseEstimate();
 
-            telemetry.addData("x ticks", pose.getX());
-            telemetry.addData("y ticks", pose.getY());
-            telemetry.addData("heading (deg)", Math.toDegrees(pose.getHeading()));
-            telemetry.addData("x cm", (pose.getX()/1.9894));
-            telemetry.addData("y cm", (pose.getY()/1.9894));
+//            telemetry.addData("x ticks", pose.getX());
+//            telemetry.addData("y ticks", pose.getY());
+//            telemetry.addData("heading (deg)", Math.toDegrees(pose.getHeading()));
+//            telemetry.addData("x cm", (pose.getX()/1.9894));
+//            telemetry.addData("y cm", (pose.getY()/1.9894));
+//            telemetry.update();
+
+            telemetry.addData("x in", pinpoint.getPosX(DistanceUnit.INCH));
+            telemetry.addData("y in", pinpoint.getPosY(DistanceUnit.INCH));
+            telemetry.addData("heading (deg)", Math.toDegrees(pinpoint.getHeading(AngleUnit.DEGREES)));
             telemetry.update();
         }
     }
